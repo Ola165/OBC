ba31fcab8bc0b19dee141abbe9b6b63a0b8f8ff8
Ola165 <ductrhoang@gmail.com>
2026-04-30 16:09:28 +0700

    Delete openbootcamp directory

--- openbootcamp/OBC.IPC/Constants.cs
+++
@@ -1,9 +0,0 @@
-using MessagePack;
-
-namespace OBC.IPC;
-
-internal static class Constants
-{
-    public static readonly MessagePackSerializerOptions SerializerOptions =
-        MessagePackSerializerOptions.Standard.WithSecurity(MessagePackSecurity.TrustedData);
-}
